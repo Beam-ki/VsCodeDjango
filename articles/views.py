16bb0819d42826b091733ce7353df5e56842d2ca
Beam-ki <skunn123@gmail.com>
2022-09-26 21:45:32 +0900

    dinner and random

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,16 @@
+import imp
 from django.shortcuts import render
+import random
 
 # Create your views here.
 def index(requset):
     return render(requset, 'index.html')
+
+def dinner(request):
+    menus = ['족발','치킨','피자','초밥']
+
+    pick = random.choice(menus)
+    context = {
+        'pick' : pick
+    }
+    return render(request, 'dinner.html',context)
